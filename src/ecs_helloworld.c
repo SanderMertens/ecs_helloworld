4dd746f1cbe29effbdc4d66710194de0360e7142
Sander Mertens <sander.mertens8@gmail.com>
2018-09-17 01:27:28 -0700

    Fix tabs

--- src/ecs_helloworld.c
+++ src/ecs_helloworld.c
@@ -6,8 +6,8 @@
 /* -- Components -- */
 
 typedef struct Position {
-		int x;
-		int y;
+    int x;
+    int y;
 } Position;
 
 typedef int Speed;
@@ -16,40 +16,40 @@ typedef int Speed;
 /* -- Systems --*/
 
 void Init(void *data[], EcsInfo *info) {
-		Position *position = data[0];
-		Speed *speed = data[1];
-		position->x = 0;
-		position->y = 0;
-		*speed = 1;
+    Position *position = data[0];
+    Speed *speed = data[1];
+    position->x = 0;
+    position->y = 0;
+    *speed = 1;
 }
 
 void Move(void *data[], EcsInfo *info) {
-		Position *position = data[0];
-		Speed speed = *(Speed*)data[1];
-		position->x += speed;
-		position->y += speed * 2;
+    Position *position = data[0];
+    Speed speed = *(Speed*)data[1];
+    position->x += speed;
+    position->y += speed * 2;
 }
 
 int main(int argc, char *argv[]) {
-		EcsWorld *world = ecs_init();
+    EcsWorld *world = ecs_init();
 
-		/* Register components and systems with world */
-		ECS_COMPONENT(world, Position);
-		ECS_COMPONENT(world, Speed);
-		ECS_SYSTEM(world, Init, EcsOnInit, Position, Speed);
-		ECS_SYSTEM(world, Move, EcsPeriodic, Position, Speed);
+    /* Register components and systems with world */
+    ECS_COMPONENT(world, Position);
+    ECS_COMPONENT(world, Speed);
+    ECS_SYSTEM(world, Init, EcsOnInit, Position, Speed);
+    ECS_SYSTEM(world, Move, EcsPeriodic, Position, Speed);
 
-		/* Create single entity with Position and Speed component */
-		EcsHandle e = ecs_new(world);
-		ecs_stage(world, e, Position_h);
-		ecs_stage(world, e, Speed_h);
-		ecs_commit(world, e);
+    /* Create single entity with Position and Speed component */
+    EcsHandle e = ecs_new(world);
+    ecs_stage(world, e, Position_h);
+    ecs_stage(world, e, Speed_h);
+    ecs_commit(world, e);
 
-		/* Progress world once a second */
-		while (true) {
-				ecs_progress(world);
-				sleep(1);
-		}
+    /* Progress world once a second */
+    while (true) {
+    		ecs_progress(world);
+    		sleep(1);
+    }
 
-	  return 0;
+    return 0;
 }
